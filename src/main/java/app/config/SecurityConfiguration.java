5495fc43ea0373b3a48cac79ed6c16d5250ba56b
Vadym Zdoryk <VZdoryk@luxoft.com>
2018-12-06 00:48:09 +0100

    disabling popup on index page load

--- src/main/java/app/config/SecurityConfiguration.java
+++ src/main/java/app/config/SecurityConfiguration.java
@@ -144,6 +144,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/authenticate").permitAll()
                 .antMatchers("/api/account/reset_password/init").permitAll()
                 //.antMatchers("/api/account/*").permitAll()
+                .antMatchers("/management/info").permitAll()
                 .antMatchers("/api/account/reset_password/finish").permitAll()
                 .antMatchers("/api/profile-info").permitAll()
                 .antMatchers("/api/**").authenticated()
